cc7577a9c6baecde887cee085765a636e343172f
Kayleonia <julie_nardino@hotmail.com>
2023-06-19 17:15:08 -0700

    Created new component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Weather from './Weather';
 import './App.css';
 
 function App() {
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from App!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Weather />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Weather(){
+    return <p>Hello from Weather!</p>;
+}
