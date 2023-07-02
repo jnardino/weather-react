aa0e290efdc0a717345f1fb6ed64a3c2185d3bd9
Kayleonia <julie_nardino@hotmail.com>
2023-07-02 15:25:47 -0700

    Deleted Header component and moved it to the AppBox

--- src/AppBox.js
+++ src/AppBox.js
@@ -6,7 +6,40 @@ import Header from "./Header";
 export default function App() {
   return (
     <div className="app-box">
-      <Header />
+      	<header>
+			<div className="row">
+				<div className="col-10 search">
+				<form className="input-group">
+					<button
+					className="btn btn-outline-secondary location-icon"
+					type="button"
+					>
+					<i className="fa-solid fa-location-dot"></i>
+					</button>
+					<input
+					type="text"
+					className="form-control search-bar"
+					placeholder="Enter a city..."
+					/>
+					<button
+					className="btn btn-outline-secondary search-icon"
+					type="submit"
+					>
+					<i className="fa-solid fa-magnifying-glass"></i>
+					</button>
+				</form>
+				</div>
+				<div className="col-2 temperature-unit">
+				<button
+					className="btn btn-outline-secondary temperature-unit-icon"
+					type="button"
+				>
+					°F
+				</button>
+				</div>
+			</div>
+			<h1>Vancouver - Canada</h1>
+    	</header>
       <div className="city-name"></div>
       <div className="row gy-3 weather-now">
         <div className="col-6 col-md-4 city">
