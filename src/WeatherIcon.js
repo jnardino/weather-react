c698314363a490a1a765500ed92b61bcd84e7d1e
Kayleonia <julie_nardino@hotmail.com>
2023-07-03 21:52:07 -0700

    Updated class to "className" in WeatherIcon component

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,28 +2,28 @@ import React from "react";
 
 export default function WeatherIcon(props) {
 	const iconMap = {
-        "01d": <i class="fa-solid fa-sun"></i>,
-        "01n": <i class="fa-solid fa-moon"></i>,
-        "02d": <i class="fa-solid fa-cloud-sun"></i>,
-        "02n": <i class="fa-solid fa-cloud-moon"></i>,
-        "03d": <i class="fa-solid fa-cloud"></i>,
-        "03n": <i class="fa-solid fa-cloud"></i>,
-        "04d": <i class="fa-solid fa-cloud"></i>,
-        "04n": <i class="fa-solid fa-cloud"></i>,
-        "09d": <i class="fa-solid fa-cloud-showers-heavy"></i>,
-        "09n": <i class="fa-solid fa-cloud-showers-heavy"></i>,
-        "10d": <i class="fa-solid fa-cloud-sun-rain"></i>,
-        "10n": <i class="fa-solid fa-cloud-moon-rain"></i>,
-        "11d": <i class="fa-solid fa-cloud-bolt"></i>,
-        "11n": <i class="fa-solid fa-cloud-bolt"></i>,
-        "13d": <i class="fa-solid fa-snowflake"></i>,
-        "13n": <i class="fa-solid fa-snowflake"></i>,
-        "50d": <i class="fa-solid fa-smog"></i>,
-        "50n": <i class="fa-solid fa-smog"></i>,
+        "01d": <i className="fa-solid fa-sun"></i>,
+        "01n": <i className="fa-solid fa-moon"></i>,
+        "02d": <i className="fa-solid fa-cloud-sun"></i>,
+        "02n": <i className="fa-solid fa-cloud-moon"></i>,
+        "03d": <i className="fa-solid fa-cloud"></i>,
+        "03n": <i className="fa-solid fa-cloud"></i>,
+        "04d": <i className="fa-solid fa-cloud"></i>,
+        "04n": <i className="fa-solid fa-cloud"></i>,
+        "09d": <i className="fa-solid fa-cloud-showers-heavy"></i>,
+        "09n": <i className="fa-solid fa-cloud-showers-heavy"></i>,
+        "10d": <i className="fa-solid fa-cloud-sun-rain"></i>,
+        "10n": <i className="fa-solid fa-cloud-moon-rain"></i>,
+        "11d": <i className="fa-solid fa-cloud-bolt"></i>,
+        "11n": <i className="fa-solid fa-cloud-bolt"></i>,
+        "13d": <i className="fa-solid fa-snowflake"></i>,
+        "13n": <i className="fa-solid fa-snowflake"></i>,
+        "50d": <i className="fa-solid fa-smog"></i>,
+        "50n": <i className="fa-solid fa-smog"></i>,
     };
 
 	if (iconMap[props.image] === undefined) {
-		return <i class="fa-solid fa-sun"></i>;
+		return <i className="fa-solid fa-sun"></i>;
 	} else {
 		return iconMap[props.image];
 	}
